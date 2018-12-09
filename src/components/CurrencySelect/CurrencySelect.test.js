82c4508f9b319c5228c56042da52e249137d68da
Evan Heisler <evanheisler@gmail.com>
2018-12-09 15:32:19 -0600

    init currency selector

---
+++ src/components/CurrencySelect/CurrencySelect.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CurrencySelect from './index';
+
+it('renders a default option', () => {
+  const wrapper = shallow(<CurrencySelect options={[]} />);
+  expect(wrapper.find('select').children()).toHaveLength(1);
+});
---
+++ src/components/CurrencySelect/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CurrencySelect = ({ options, change }) => {
+  return (
+    <select
+      className="bg-white"
+      defaultValue="Select"
+      onChange={e => change(e.target.value)}
+    >
+      <option value="Select" disabled>
+        Select…
+      </option>
+      {options.map(option => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+CurrencySelect.propTypes = {
+  options: PropTypes.array.isRequired
+};
+
+export default CurrencySelect;

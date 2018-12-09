11b416be8d01481bc506f89759378698cf842d3d
Evan Heisler <evanheisler@gmail.com>
2018-12-09 15:37:17 -0600

    fetch and store data for table

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,5 @@
-import { SET_EXCHANGE_RATES } from './actionTypes';
+import { SET_EXCHANGE_RATES, SET_BASE } from './actionTypes';
+import { handleJSONResponse } from 'utils';
 
 export const setExchangeRates = data => ({
   type: SET_EXCHANGE_RATES,
@@ -6,3 +7,21 @@ export const setExchangeRates = data => ({
     ...data
   }
 });
+
+export const setBase = base => ({
+  type: SET_BASE,
+  payload: base
+});
+
+export const getExchangeData = (cur = 'EUR') => {
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.exchangeratesapi.io/latest?base=${cur}`)
+      .then(handleJSONResponse)
+      .then(resp => {
+        resolve(resp);
+      })
+      .catch(err => {
+        reject(new Error(err));
+      });
+  });
+};
--- src/screens/ExchangeRates/index.js
+++ src/screens/ExchangeRates/index.js
@@ -1,40 +1,107 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setExchangeRates } from 'redux/actions';
+import { setExchangeRates, setBase, getExchangeData } from 'redux/actions';
 import CurrencyTable from 'components/CurrencyTable';
+import CurrencySelect from 'components/CurrencySelect';
+import Modal from 'components/Modal';
 
 class ExchangeRates extends Component {
   state = {
+    status: '',
     loading: false,
-    rates: []
+    currencies: [],
+    modal: {
+      show: false,
+      msg: ''
+    }
   };
 
   componentDidMount() {
-    fetch(`https://api.exchangeratesapi.io/latest?base=EUR`)
-      .then(resp => resp.json())
-      .then(resp => {
-        this.props.setExchangeRates(resp);
-      });
+    this.setState(
+      {
+        loading: true,
+        status: 'Checking Service…'
+      },
+      () => {
+        getExchangeData()
+          .then(res => {
+            this.setState({
+              loading: false,
+              currencies: Object.keys(res.rates).map(cur => cur),
+              status: 'Please select a base currency'
+            });
+          })
+          .catch(err => {
+            this.setState({
+              loading: false,
+              modal: {
+                show: true,
+                msg:
+                  'Sorry we are having trouble connecting or that currency is not available. Please try again.'
+              }
+            });
+          });
+      }
+    );
   }
 
+  handleCurrencyChange = base => {
+    if (this.props.availableRates[base]) {
+      this.props.setBase(base);
+    } else {
+      getExchangeData(base).then(resp => {
+        this.props.setExchangeRates(resp);
+        this.props.setBase(base);
+      });
+    }
+  };
+
   render() {
-    const { loading, rates } = this.state;
+    const { loading, status, currencies, modal } = this.state;
 
     return (
-      <div>
-        <CurrencyTable loading={loading} rows={rates} />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row mt-5 mb-2">
+              <div className="col-4 ml-auto pr-0 text-right">
+                <CurrencySelect
+                  options={currencies}
+                  change={this.handleCurrencyChange}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10 mx-auto py-5 bg-white rounded">
+            <CurrencyTable loading={loading} status={status} />
+          </div>
+        </div>
+        {modal.show && (
+          <Modal
+            msg={modal.msg}
+            action={() => window.location.reload()}
+            actionLabel="Refresh"
+            dismiss={() => {
+              this.setState(prevState => ({
+                modal: {
+                  show: !prevState.modal.show
+                }
+              }));
+            }}
+          />
+        )}
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    currentRates: state.currentRates
-  };
-};
+const mapStateToProps = state => ({
+  availableRates: state.currentRates
+});
 
 export default connect(
   mapStateToProps,
-  { setExchangeRates }
+  { setExchangeRates, setBase }
 )(ExchangeRates);
